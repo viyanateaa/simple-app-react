60215166fb00b8fcae78f0fce1827f6317fd759b
viyanateaa <viyanateaa@gmail.com>
2017-09-16 12:24:08 +0200

    remove import Task from index file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import Task from './Task';
+ 
 import registerServiceWorker from './registerServiceWorker';
 
 import 'bootstrap/dist/css/bootstrap.css';
