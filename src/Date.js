3ece5d35e0e15d09da7531541c64c33b1311dcb6
viyanateaa <viyanateaa@gmail.com>
2017-09-16 12:22:19 +0200

    create Date file, dynamic file using Moment library

---
+++ src/Date.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import Moment from 'moment';
+
+
+class Date extends Component {
+  render() {
+    return(
+      <div>
+        <h1> {Moment().format('dddd')}</h1>
+        <p>{Moment().format('LL')}</p>
+      </div>
+    );
+  }
+}
+export default Date;
