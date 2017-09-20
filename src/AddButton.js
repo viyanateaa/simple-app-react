f4bb08d68ddb501fdadbe2e93e65abc4a8dda136
viyanateaa <viyanateaa@gmail.com>
2017-09-20 22:22:13 +0200

    add plus function

--- src/AddButton.js
+++ src/AddButton.js
@@ -1,21 +1,26 @@
-import React, {Component} from 'react';
-import FontAwesome from 'react-fontawesome';
-import {Row, Col, Button} from 'react-bootstrap';;
 
-class AddButton extends Component{
-  render(){
-    return(
-      <div>
-        <Row>
-               <Col xs={10}></Col>
-               <Col xs={2}>
-                 <Button bsStyle="danger" bsSize="large">
-                   <FontAwesome name='plus' />
-                 </Button>
-               </Col>
-             </Row>
-      </div>
-    );
-  }
+import React, {Component} from 'react';
+import {Row, Col, Button} from 'react-bootstrap';
+import FontAwesome from 'react-fontawesome';
+
+class AddButton extends Component {
+    handleClick(){
+      this.props.onClick();
+    }
+    render() {
+        return (
+            <div>
+              <Row>
+                <Col xs={10}></Col>
+                <Col xs={2}>
+                  <Button bsStyle="danger" bsSize="large" onClick={this.handleClick.bind(this)}>
+                    <FontAwesome name='plus' />
+                  </Button>
+                </Col>
+              </Row>
+            </div>
+        );
+    }
 }
+
 export default AddButton;
